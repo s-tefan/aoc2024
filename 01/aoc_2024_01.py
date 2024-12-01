f0642e08ce5aaa7a3e63a9a9fe4be5a68d570c34
s-tefan <github@k-son.se>
2024-12-01 10:32:15 +0100

    day 1 finished

---
+++ 01/aoc_2024_01.py
@@ -0,0 +1,23 @@
+def read_input(filename):
+    with open(filename) as f:
+        return [line.rstrip() for line in f.readlines()]
+
+def fix(lines):
+    lists = [[],[]]
+    for line in lines:
+        ap = line.split()
+        for k, bep in enumerate(lists):
+            bep.append(int(ap[k]))
+    return lists
+
+def partone(lists):
+    sorted_lists = list(map(sorted, lists))
+    return sum(abs(sorted_lists[0][k] - sorted_lists[1][k]) for k in range(len(sorted_lists[0])))
+
+def parttwo(lists):
+    return sum(no * lists[1].count(no) for k, no in enumerate(lists[0]))
+
+lists = fix(read_input("input.txt"))
+print(partone(lists))
+print(parttwo(lists))
+      
