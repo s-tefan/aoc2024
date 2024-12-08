24c30d5cfd10134adb633d9ec9524f2fa7206ddb
s-tefan <github@k-son.se>
2024-12-08 16:38:57 +0100

    -

---
+++ 10/aoc_2024_10.py
@@ -0,0 +1,19 @@
+'''
+AoC 2024 Day 10
+'''
+def read_input(filename):
+    with open(filename) as f:
+        return [line.rstrip() for line in f.readlines()]
+
+def fix(lines):
+    return lines
+
+def partone(data):
+    return None
+
+def parttwo(data):
+    return None
+
+data = fix(read_input('10/test.txt'))
+print(partone(data))
+print(parttwo(data))
